d7fdf8c282cd346bf1b73fdc7ddffa2c9da8cba1
daniel <57467819+DanielCW7@users.noreply.github.com>
2023-02-25 13:20:48 -0600

    created a story component, changed min height, created news component

---
+++ src/components/story.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Story = () => {
+    return (
+        <div>
+            <h3> headline </h3>
+            <p> this text should probably end after 20 words and then have an elipsis </p>
+        </div>
+    )
+}
+
+export default Story
