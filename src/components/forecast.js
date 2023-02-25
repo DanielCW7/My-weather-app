7bc2f72471a5b768785c3864a1b835019138681c
daniel <57467819+DanielCW7@users.noreply.github.com>
2023-02-25 14:22:26 -0600

    adjusted height ratios

--- src/components/forecast.js
+++ src/components/forecast.js
@@ -10,11 +10,11 @@ const Forecast = () => {
 
     return (
         <div className="bg-city-pattern bg-cover bg-center bg-fixed min-h-screen flex flex-col justify-center">
-            <div className="flex flex-row justify-around min-h-50 p-10">
+            <div className="flex flex-row justify-around min-h-30 p-10">
                 <Form today={setVal} week={setVal2}/>
                 <Today val={val}/>
             </div>
-            <div className="flex flex-row justify-around min-h-50 p-10">
+            <div className="flex flex-row justify-around min-h-70 p-10">
                 <Week val2={val2}/>
             </div>
         </div>
